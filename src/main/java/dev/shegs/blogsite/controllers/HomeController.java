e7b9d2f43a3e7679445883e0d9b1ece6a335ed04
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-10-29 19:17:16 +0100

    Using Model to pass data to the frontend

--- src/main/java/dev/shegs/blogsite/controllers/HomeController.java
+++ src/main/java/dev/shegs/blogsite/controllers/HomeController.java
@@ -1,4 +1,25 @@
 package dev.shegs.blogsite.controllers;
 
+import dev.shegs.blogsite.models.Post;
+import dev.shegs.blogsite.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/")
+    public String Homepage(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "homepage";
+    }
+
 }
