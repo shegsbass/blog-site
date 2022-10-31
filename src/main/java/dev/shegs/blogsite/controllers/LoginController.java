2e5862aa38a69d7c2412f8568cc83e3584297856
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2022-10-31 16:22:03 +0100

    Persisting user registration data from view to DB

--- src/main/java/dev/shegs/blogsite/controllers/LoginController.java
+++ src/main/java/dev/shegs/blogsite/controllers/LoginController.java
@@ -1,14 +1,19 @@
 package dev.shegs.blogsite.controllers;
 
+import dev.shegs.blogsite.models.UserAccount;
+import dev.shegs.blogsite.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String getLoginPage(){
         return "login";
     }
-
 }
--- src/main/java/dev/shegs/blogsite/controllers/RegisterController.java
+++ src/main/java/dev/shegs/blogsite/controllers/RegisterController.java
@@ -1,13 +1,29 @@
 package dev.shegs.blogsite.controllers;
 
+import dev.shegs.blogsite.models.UserAccount;
+import dev.shegs.blogsite.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
+    @Autowired
+    private UserAccountService userAccountService;
 
     @GetMapping("/register")
-    public String getRegisterPage(){
+    public String getRegisterPage(Model model){
+        UserAccount userAccount = new UserAccount();
+        model.addAttribute("userAccount", userAccount);
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String saveNewUser(@ModelAttribute UserAccount userAccount){
+        userAccountService.saveUser(userAccount);
+        return "redirect:/";
     }
 }
